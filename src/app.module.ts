010ed7c09e0235616f192f198b60bcf4271a0ce1
Paola Alejandra Apaza Quispe <paolaapaza.dev@gmail.com>
2025-12-27 00:13:39 -0400

    refactor: migrar de MySQL a PostgreSQL y agregar configuración

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,31 @@ import { AppService } from './app.service';
 import { SeriesModule } from './series/series.module';
 import { EpisodiosModule } from './episodios/episodios.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'user_crud',
-      password: 'root',
-      database: 'db_nestflix',
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT || '5432'),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
+      ssl: process.env.POSTGRES_SSL === 'true',
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
     }),
     SeriesModule,
     EpisodiosModule,
